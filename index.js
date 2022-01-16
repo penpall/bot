34f959ae896d15dbb1365e5b01bb1b6ebe9d563e
penpall <danielekholm04@gmail.com>
2022-01-16 16:10:23 +0100

    initialize github repository

---
+++ index.js
@@ -0,0 +1,22 @@
+const Discord = require("discord.js")
+require("dotenv").config()
+
+const client = new Discord.Client({
+    intents: [
+        "GUILDS",
+        "GUILD_MESSAGES"
+    ]
+})
+
+client.on("ready", () => {
+    console.log(`Logged in as ${client.user.tag}`)
+})
+
+client.on("messageCreate", (message) => {
+    if (message.author.bot == true) return;
+    if (message.content == "hi"){
+        message.reply("hi")
+    }
+})
+
+client.login(process.env.TOKEN)
